7cb32e9b1dd3964df7162153bf2b630b274a2cb3
Gormysha <gormysha@gmail.com>
2026-02-05 22:29:41 +0300

    Revert
    This reverts commit 86f3dafbde4df26bfd8baf3e1a1b7198f41d27ce.

--- utils/test.js
+++
@@ -1,2 +0,0 @@
-import { parseExpression } from "./tokenizer";
-
--- utils/triplet.js
+++
@@ -1,9 +0,0 @@
-class Triplet {
-    constructor(c) {
-        let vector = typeof c === `number` ? [Math.floor(c / 65536), Math.floor((c % 65536) / 256), c % 256] : c;
-        let value = Array.isArray(c) ? c[2] + 256 * c[1] + 65536 * c[0] : c;
-        Object.assign(this, { vector, value });
-    }
-}
-
-export {Triplet}
